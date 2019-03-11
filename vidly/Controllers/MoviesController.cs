a69c6864fac902364505f037dd4a4aa57c5779e3
Joseph Niccoli <niccoli.joseph592@outlook.com>
2019-03-11 14:45:06 -0700

    Update From 3/11/2019

--- vidly/Controllers/MoviesController.cs
+++ vidly/Controllers/MoviesController.cs
@@ -17,14 +17,20 @@ namespace vidly.Controllers
 	        {
 		        Name = "Shrek!"
 	        };
-		
-	        //return Content("Hello World");
-	        //return HttpNotFound(); // displays standard 404 error
-			//return new ViewResult();
-			//return View(movie); // add movie model to render it  
-			//return new EmptyResult();
-			return RedirectToAction("Index", "Home", new {page = 1, sortBy = "name"});
 
+			//return Content("Hello World");
+			//return HttpNotFound(); // displays standard 404 error
+			//return new EmptyResult();
+			//return RedirectToAction("Index", "Home", new {page = 1, sortBy = "name"});
+			//return new ViewResult();
+			return View(movie); // add movie model to render it  
+								
+
+		}
+
+        public ActionResult Edit(int id)
+        {
+	        return Content("id =  " + id);
         }
     }
 }
